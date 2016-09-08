b730e640e999199e24301a8a58f0535cf49f44eb
Akimitsu Takano <Akimitsu Takano>
2016-09-08 16:19:10 +0900

    change

--- golang/sample/sample1.go
+++ golang/sample/sample1.go
@@ -6,18 +6,18 @@ import (
 	"../sample2" // サンプル：自分の別パッケージを指定する場合の相対パス。ただしGOPATH配下では使えない
 )
 
-// S1
+// S1:string
 func S1(x, y int) string {
 	s1 := fmt.Sprintf("%d:%d", x, y)
 	return s1
 }
 
-// S2
+// S2: int
 func S2(x, y int) int {
 	return 10
 }
 
-// S3
+// S3: string
 func S3(x, y int) string {
 	s1 := fmt.Sprintf("%d:%d:%d", x, y, sample2.S2_2())
 	return s1
