230dbe48ab96893e3f1317475e0d3cdfb1546075
Akimitsu Takano <takano@newbiz.acmos.jp>
2017-07-20 19:50:52 +0900

    dr-test04

---
+++ dummy/dummt_test.go
@@ -0,0 +1,12 @@
+package dummy
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAaa(t *testing.T) {
+	fmt.Println("aaa")
+
+	// t.Error("aaa")
+}
---
+++ dummy/dummy01.go
@@ -0,0 +1,10 @@
+package dummy
+
+import "fmt"
+
+func aaa() {
+
+	fmt.Println("test")
+	fmt.Println("test2")
+
+}
